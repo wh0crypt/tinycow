dbb5ffc37531db5f295726dee847b735309ca9d6
wh0crypt <wh0crypt@computer.org>
2026-05-15 13:45:09 +0100

    feat: implement all instructions besides jumps

--- src/tinymoo.c
+++ src/tinymoo.c
@@ -1,7 +1,9 @@
 // https://esolangs.org/wiki/COW
 
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 
 
@@ -28,16 +30,121 @@ enum
     COPY_REG        = 0x09,
     PRINT_INT       = 0x0A,
     READ_INT        = 0x0B,
-    UNK             = 0xFF
+    INV             = 0xFF
 } Instruction;
 
 
+void run(Program* program);
 uint8_t get_op_from_str(char* str);
 
 
 int main()
 {
     return 0;
+}
+
+
+#define ASSERT_TAPE_FULL()                          \
+    do                                              \
+    {                                               \
+        if (dp >= (MAX_PROG_LEN - 1)) {             \
+            fprintf(stderr, "error: tape full\n");  \
+            return;                                 \
+        }                                           \
+    } while (0)
+
+#define ASSERT_TAPE_EMPTY()                             \
+    do                                                  \
+    {                                                   \
+        if (dp <= 0)                                    \
+        {                                               \
+            fprintf(stderr, "error: tape empty\n");     \
+            return;                                     \
+        }                                               \
+    } while (0)
+
+#define ASSERT_PC_OUTBOUND(pc, len)                                                         \
+    do                                                                                      \
+    {                                                                                       \
+        if (pc >= len)                                                                      \
+        {                                                                                   \
+            fprintf(stderr, "error: pc value '%02lx' out-of-bounds (%02lx)\n", pc, len);    \
+            return;                                                                         \
+        }                                                                                   \
+    } while (0)
+
+void run(Program* program)
+{
+    uint16_t pc         = 0;
+    int      dp         = 0;
+    uint8_t  reg        = 0;
+    uint8_t  reg_empty  = 1;
+    uint8_t  tape[MAX_PROG_LEN];
+
+    while (pc < program->len)
+    {
+        uint8_t opcode = get_op_from_str(program->program[pc]);
+        SWITCH:
+        switch (opcode)
+        {
+            case JUMP_START:
+                // TODO
+                break;
+            case BACK:
+                ASSERT_TAPE_EMPTY();
+                --dp; ++pc;
+                break;
+            case FORWARD:
+                ASSERT_TAPE_FULL();
+                ++dp; ++pc;
+                break;
+            case EXEC:
+                if (tape[dp] > READ_INT || tape[dp] == EXEC)
+                {
+                    fprintf(stderr, "error: invalid EXEC instruction '0x%02lx' at '0x%02lx'\n", tape[dp], pc);
+                    exit(1);
+                }
+                opcode = tape[dp];
+                goto SWITCH;
+                break;
+            case IO_IF_ZERO:
+                if (tape[dp] == 0) tape[dp] = (uint8_t)getchar();
+                else putchar(tape[dp]);
+                break;
+            case DEC:
+                --tape[dp]; ++pc;
+                break;
+            case INC:
+                ++tape[dp]; ++pc;
+                break;
+            case JUMP_IF_ZERO:
+                // TODO
+                break;
+            case SET_ZERO:
+                tape[dp] = 0; ++pc;
+                break;
+            case COPY_REG:
+                if (reg_empty == 1)
+                {
+                    reg = tape[dp];
+                    reg_empty = 0;
+                }
+                else
+                {
+                    tape[dp] = reg;
+                    reg_empty = 1;
+                }
+                ++pc;
+                break;
+            case PRINT_INT:
+                printf("%d", tape[dp]); ++pc;
+                break;
+            case READ_INT:
+                scanf("%" SCNu8, tape + dp); ++pc;
+                break;
+            default: ++pc;
+        }
+    }
 }
 
 uint8_t get_op_from_str(char* str)
@@ -54,6 +161,6 @@ uint8_t get_op_from_str(char* str)
     if (!strcmp(str, "MMM")) return COPY_REG;
     if (!strcmp(str, "OOM")) return PRINT_INT;
     if (!strcmp(str, "oom")) return READ_INT;
-    return UNK;
+    return INV;
 }
 
