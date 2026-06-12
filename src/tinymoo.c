a2a7ca1baa3b2d0933f073721ad330698fa2d420
wh0crypt <wh0crypt@computer.org>
2026-06-13 00:30:37 +0100

    fix: add actual help message to show_help

--- src/tinymoo.c
+++ src/tinymoo.c
@@ -14,27 +14,27 @@ int main(int argc, char* argv[])
     if (argc < 2)
     {
         show_usage();
-        return 0;
+        return EXIT_SUCCESS;
     }
 
     if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help"))
     {
-        printf("to be implemented\n"); // TODO: change to an actual help message
-        return 0;
+        show_help();
+        return EXIT_FAILURE;
     }
 
     Program* p = create_prog_from_file(argv[1]);
     if (!p)
     {
         fprintf(stderr, "error: program is empty\n");
-        return 1;
+        return EXIT_FAILURE;
     }
 
     if (!p->len)
     {
         fprintf(stderr, "error: program is empty\n");
         free_program(p);
-        return 1;
+        return EXIT_FAILURE;
     }
 
     Registers* reg = malloc(sizeof(Registers));
@@ -42,24 +42,36 @@ int main(int argc, char* argv[])
     {
         fprintf(stderr, "error: could not create registers\n");
         free_program(p);
-        return 1;
+        return EXIT_FAILURE;
     }
 
     reg->pc = 0; reg->dp = 0;
     run(p, reg);
     free_program(p);
     free(reg);
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 
 void show_usage()
 {
-    printf("Usage: tinycow [-h|--help] <FILE>\nTry 'tinycow --help' for more information.\n");
+    printf(
+        "Usage: tinycow [-h|--help] <FILE>\n"
+        "Try 'tinycow --help' for more information.\n"
+    );
 }
 
 void show_help()
 {
-    // TODO: print an actual help message
+    printf(
+        "tinycow -- a tiny virtual machine for the COW esoteric language.\n\n"
+        "Usage: tinycow [-h|--help] <FILE>\n\n"
+        "OPTIONS:\n"
+        "    <FILE>\tSource file to execute.\n"
+        "    -h, --help\tPrint this message.\n\n"
+        "EXAMPLES:\n"
+        "    tinycow --help\n"
+        "    tinycow files/hello_world.cow\n"
+    );
 }
 
